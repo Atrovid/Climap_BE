924ae219e7e75741e858b3962f182c8319da66da
David Guo <=>
2024-01-17 14:19:29 +0100

    Service : get sound data

--- Back-End/src/main/java/ENSICAEN/intensive_project/Climap/database/repository/SoundRepository.java
+++ Back-End/src/main/java/ENSICAEN/intensive_project/Climap/database/repository/SoundRepository.java
@@ -2,6 +2,11 @@ package ENSICAEN.intensive_project.Climap.database.repository;
 
 import ENSICAEN.intensive_project.Climap.database.entities.SoundEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SoundRepository extends JpaRepository<SoundEntity, Integer> {
+    @Query("SELECT s._decibel FROM Sound s")
+    List<Object> getDecibel();
 }
