85603bfa1a14d38eead352600131db0a40dc0f1a
malo <malo.boulenger@ecole.ensicaen.fr>
2024-01-18 16:26:55 +0100

    Code cleaned

--- Back-End/src/main/java/ENSICAEN/intensive_project/Climap/database/controller/AddController.java
+++ Back-End/src/main/java/ENSICAEN/intensive_project/Climap/database/controller/AddController.java
@@ -1,20 +1,9 @@
 package ENSICAEN.intensive_project.Climap.database.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Base64;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -23,11 +12,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class AddController {
 
     @PostMapping
-    public String recevoirFichier(@RequestBody String file) throws IOException {
-        // Traitement du fichier ici
+    public void receiveFileFromDevice(@RequestBody String file) throws IOException {
 
         String existingJsonFilePath = "src/main/java/ENSICAEN/intensive_project/Climap/database/resource/Device.json";
-        String newJsonFilePath = "src/main/java/ENSICAEN/intensive_project/Climap/database/resource/output.json";
 
         ObjectMapper mapper = new ObjectMapper();
 
@@ -35,7 +22,5 @@ public class AddController {
         var node2 = mapper.readerForUpdating(node1).readTree(file);
 
         mapper.writeValue(new File(existingJsonFilePath), node2);
-
-        return "Fichier reçu avec succès";
     }
 }
