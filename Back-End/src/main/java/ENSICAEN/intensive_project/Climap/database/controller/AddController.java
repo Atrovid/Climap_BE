beb493958025e010384d34fbce19916935549923
malo <malo.boulenger@ecole.ensicaen.fr>
2024-01-18 16:14:15 +0100

    Addcontroller: merge json file received with json file already existing. Done by http using python with url = 'http://localhost:8080/add'. Dependency have been add.

--- Back-End/src/main/java/ENSICAEN/intensive_project/Climap/database/controller/AddController.java
+++ Back-End/src/main/java/ENSICAEN/intensive_project/Climap/database/controller/AddController.java
@@ -25,50 +25,16 @@ public class AddController {
     @PostMapping
     public String recevoirFichier(@RequestBody String file) throws IOException {
         // Traitement du fichier ici
-        System.out.println(file);
 
         String existingJsonFilePath = "src/main/java/ENSICAEN/intensive_project/Climap/database/resource/Device.json";
         String newJsonFilePath = "src/main/java/ENSICAEN/intensive_project/Climap/database/resource/output.json";
 
         ObjectMapper mapper = new ObjectMapper();
 
-        try {
-            // Read the existing JSON file
-            JsonNode existingJson = mapper.readTree(new File(existingJsonFilePath));
+        var node1 = mapper.readTree(new File(existingJsonFilePath));
+        var node2 = mapper.readerForUpdating(node1).readTree(file);
 
-            System.out.println("existingJson");
-            System.out.println(existingJson);
-
-            // Read the new JSON file
-            //JsonNode newJson = mapper.readTree(new File(newJsonFilePath));
-            JsonNode newJson = mapper.readTree(file);
-
-            System.out.println("newJson");
-            System.out.println(newJson);
-
-            // Check if the existing JSON is an array
-            if (existingJson.isArray()) {
-                // If it's an array, add the new JSON to it
-                ArrayNode existingArray = (ArrayNode) existingJson;
-                existingArray.add(newJson);
-            } else {
-                // If it's not an array, create a new array and add both JSON objects
-                ArrayNode newArray = mapper.createArrayNode();
-                newArray.add(existingJson);
-                newArray.add(newJson);
-                existingJson = newArray;
-            }
-
-            System.out.println("existingJson");
-            System.out.println(existingJson);
-
-            // Write the combined JSON back to the existing file
-            mapper.writeValue(new File(existingJsonFilePath), existingJson);
-
-            System.out.println("JSON data combined and updated in Device.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        mapper.writeValue(new File(existingJsonFilePath), node2);
 
         return "Fichier reçu avec succès";
     }
